5ebcf3db5c174cd3a90dd34dcf0d449845cb65ec
PLAN CHRISTIAN ROMEO CRUZ <plan002@mymail.sim.edu.sg>
2021-09-24 19:51:40 +0800

    r_studio_homework

---
+++ readme.md
@@ -0,0 +1,12 @@
+# First assignment
+
+## Markdown Previews
+
+**Christian Romeo Plan**
+*Christian Romeo Plan*
+
+Assignments
+* R_studio Folder
+** R-Scripts
+* Spyder Folder
+** 
---
+++ spyder/homework.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 07:59:07 2021
+
+@author: chris
+"""
+
+for i in range(1,11):
+        print(i)
+
+
+
+for i in range(1,11):
+        print(i, end = " ")        

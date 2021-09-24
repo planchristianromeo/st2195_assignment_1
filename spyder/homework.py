ec214f64629e30e4b8ff6895dc933c45f6fc4c65
PLAN CHRISTIAN ROMEO CRUZ <plan002@mymail.sim.edu.sg>
2021-09-24 20:57:21 +0800

    spyder_homework

---
+++ spyder/homework.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 20:43:36 2021
+
+@author: chris
+"""
+
+for i in range(1,11):
+        print(i)
+
+
+
+for i in range(1,11):
+        print(i, end = " ")
